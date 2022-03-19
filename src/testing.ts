6476d8e8a3503fcfbc513fbfd69e49152238b614
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-03-19 01:28:01 +0100

    Removes unused functions

--- src/testing.ts
+++ src/testing.ts
@@ -6,28 +6,11 @@ export function parse(source: string | Source): DocumentNode {
   return parseBase(source, { noLocation: true });
 }
 
-export function expectASTToMatchGraphQL(
-  actualAST: DocumentNode,
-  expectedGraphQL: string | Source
-): void {
-  const expectedAST = parse(expectedGraphQL);
-  expectASTToMatch(actualAST, expectedAST);
-}
-
 function expectASTToMatch(
   actualAST: DocumentNode,
   expectedAST: DocumentNode
 ): void {
   expect(actualAST).toMatchObject(expectedAST as any);
-}
-
-export async function expectGraphQLFileToMatch(
-  filePath: string,
-  expectedSource: string
-): Promise<void> {
-  const schema = await readFile(filePath);
-  const ast = parse(schema);
-  expectASTToMatchGraphQL(ast, expectedSource);
 }
 
 export async function expectGraphQLFilesToMatch(
